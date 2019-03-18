eadef25421e3b2e5acef12ce40b2a5b1958f1dee
MaxTitovitch <maxtitovitch@mail.ru>
2019-03-19 01:39:02 +0300

    1.2 Welcome Rout Management

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,8 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Мебель: интернет магазин мебели!</title>
+        <link rel="shortcut icon" href="{{ asset('main_ico.ico') }}" type="image/x-icon">
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
@@ -81,17 +82,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    MetaMebelModern
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://vk.com/forever_best_in_the_world">Vk</a>
+                    <a href="https://github.com">GitHub</a>
+                    <a href="mailto:maxtitovitch@mail.ru">Почта</a>
                 </div>
             </div>
         </div>
